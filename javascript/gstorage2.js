b01c5fec41e8027c40e1ec1d0855f97417a845a1
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2022-11-04 20:15:49 -0500

    NTS

--- javascript/gstorage2.js
+++ javascript/gstorage2.js
@@ -5,7 +5,7 @@ class gStorage {
      * booleano
      * @param key - La clave del valor a almacenar.
      * @param value [e7efa9b9-4e0f-0626-5419-6b627e28bbca] - El valor que se almacenará en
-     * la sesión.
+     * sessionStorage.
      * @returns El valor de la clave en el sessionStorage.
      */
     static session(key, value = 'e7efa9b9-4e0f-0626-5419-6b627e28bbca') {
@@ -36,6 +36,64 @@ class gStorage {
             return;
         } else {
             value = sessionStorage.getItem(key);
+            let kv = String(value).split(':');
+            switch (kv[0]) {
+                case 'undefined':
+                    return undefined;
+                case 'boolean':
+                    return value.replace('boolean:', '') == 'true' ? true : false;
+                case 'number':
+                    return Number(value.replace('number:', ''));
+                case 'string':
+                    return value.replace('string:', '');
+                case 'bigint':
+                    return BigInt(value.replace('bigint:', ''));
+                case 'object':
+                    value = value.replace('object:', '');
+                    return value == 'null' ? null : JSON.parse(value);
+                default:
+                    return value;
+            }
+        }
+    }
+
+    /**
+     * Le permite almacenar y recuperar cualquier tipo de datos en localStorage manteniendo
+     * la integridad de su valor, si es objeto, retorna un objeto, si es booleano retorna un
+     * booleano
+     * @param key - La clave del valor a almacenar.
+     * @param value [e7efa9b9-4e0f-0626-5419-6b627e28bbca] - El valor que se almacenará en
+     * localStorage.
+     * @returns The value of the key in localStorage.
+     */
+    static local(key, value = 'e7efa9b9-4e0f-0626-5419-6b627e28bbca') {
+        if (value != 'e7efa9b9-4e0f-0626-5419-6b627e28bbca') {
+            switch (typeof value) {
+                case 'undefined':
+                    localStorage.setItem(key, `undefined:${value}`);
+                    break;
+                case 'boolean':
+                    localStorage.setItem(key, `boolean:${value}`);
+                    break;
+                case 'number':
+                    localStorage.setItem(key, `number:${value}`);
+                    break;
+                case 'string':
+                    localStorage.setItem(key, `string:${value}`);
+                    break;
+                case 'bigint':
+                    localStorage.setItem(key, `bigint:${value}`);
+                    break;
+                case 'object':
+                    localStorage.setItem(key, `object:${JSON.stringify(value)}`);
+                    break;
+                default:
+                    localStorage.setItem(key, value);
+                    break;
+            }
+            return;
+        } else {
+            value = localStorage.getItem(key);
             let kv = String(value).split(':');
             switch (kv[0]) {
                 case 'undefined':
