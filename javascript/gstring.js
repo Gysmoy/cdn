7d87c6b52e911b09791123512ea18e097e59f18f
SoDe World <sodeworld@gmail.com>
2023-02-28 17:08:53 -0500

    NTS

--- javascript/gstring.js
+++ javascript/gstring.js
@@ -45,3 +45,43 @@ String.prototype.split2 = function (separator, { regex, structure } = {}) {
         })
         .filter(Boolean);
 };
+
+/**
+ * Convierte una cadena CSV en formato JSON.
+ *
+ * @function
+ * @memberof String.prototype
+ * @param {string} [separator=','] - El separador de campo utilizado en la cadena CSV.
+ * @param {number} [headers_pos=0] - La posición de la fila de encabezado en la cadena CSV.
+ * @returns {Array<Object>} - Un array de objetos JSON, donde cada objeto representa una fila de la cadena CSV.
+ * @throws {TypeError} - Si la función se llama sobre un valor que no es una cadena.
+ */
+String.prototype.csvToJson = function (separator = ',', headers_pos = 0) {
+    let csv = this.toString();
+    const lines = csv.trim().split("\n");
+    const headers = lines[headers_pos].split(separator);
+    const result = [];
+    for (let i = (headers_pos + 1); i < lines.length; i++) {
+        const obj = {};
+        const currentLine = lines[i].split(separator);
+        let cursor = 0;
+        if (!currentLine.every(c => c === "")) { // Validar si la fila está vacía
+            for (let j = 0; j < headers.length; j++) {
+                if (currentLine[cursor].startsWith('"')) {
+                    let field = currentLine[cursor].substring(1);
+
+                    while (!currentLine[cursor].endsWith('"')) {
+                        cursor++;
+                        field += `${separator}${currentLine[cursor]}`;
+                    }
+                    obj[headers[j]] = field.slice(0, -1);
+                } else {
+                    obj[headers[j]] = currentLine[cursor];
+                }
+                cursor++;
+            }
+            result.push(obj);
+        }
+    }
+    return result;
+}
