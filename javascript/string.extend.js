c201a45e7b4f3bd251384f7d5ac60b29c4bd2159
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-03-02 17:03:21 -0500

    NTS

---
+++ javascript/string.extend.js
@@ -0,0 +1,107 @@
+/**
+ * Crea un objeto a partir de una cadena de texto que coincide con una expresión regular y una estructura de propiedades.
+ *
+ * @function
+ * @memberof String.prototype
+ * @param {RegExp} regex - La expresión regular para buscar en la cadena de texto.
+ * @param {string} structure - Una cadena de texto con la estructura de propiedades del objeto separadas por comas.
+ * @returns {Object|null} - Un objeto con las propiedades especificadas en la estructura o null si no se encuentra una coincidencia.
+ */
+String.prototype.matchAndCreateObject = function (regex, structure) {
+    try {
+        let matched = this.match(regex);
+        let object = {};
+
+        structure.split(",").forEach((s, i) => {
+            object[s.trim()] = matched[i + 1].trim();
+        });
+
+        return object;
+    } catch (error) {
+        return null;
+    }
+}
+
+/**
+ * Divide una cadena de texto en un arreglo y opcionalmente crea objetos a partir de cada elemento utilizando una expresión regular y una estructura de propiedades.
+ *
+ * @function
+ * @memberof String.prototype
+ * @param {string} separator - El separador utilizado para dividir la cadena de texto en un arreglo.
+ * @param {object} [options] - Un objeto con opciones adicionales.
+ * @param {RegExp} [options.regex] - La expresión regular utilizada para crear un objeto a partir de cada elemento.
+ * @param {string} [options.structure] - Una cadena de texto con la estructura de propiedades de cada objeto separadas por comas.
+ * @returns {Array} - Un arreglo con los elementos de la cadena de texto y, si se especifica la opción "regex", objetos creados a partir de cada elemento.
+ */
+String.prototype.split2 = function (separator, { regex, structure } = {}) {
+    let array = this.split(separator);
+
+    return array
+        .map(e => {
+            if (!regex) {
+                return e.trim();
+            } else {
+                return e.trim().matchAndCreateObject(regex, structure);
+            }
+        })
+        .filter(Boolean);
+};
+
+/**
+ * Convierte una cadena de texto en formato CSV en un arreglo de objetos JSON.
+ *
+ * @function
+ * @memberof String.prototype
+ * @param {string} [separator=','] - El separador utilizado para separar los campos de la fila.
+ * @param {number} [headers_pos=0] - La posición de la fila que contiene los encabezados de las columnas.
+ * @returns {Array} - Un arreglo de objetos JSON con los datos del archivo CSV.
+ */
+String.prototype.csvToJson = function (separator = ',', headers_pos = 0) {
+    let csv = this.toString();
+    const lines = csv.trim().split("\n");
+    const headers = lines[headers_pos].split(separator);
+    const result = [];
+    for (let i = (headers_pos + 1); i < lines.length; i++) {
+        const obj = {};
+        const currentLine = lines[i].split(separator);
+        let cursor = 0;
+        if (!currentLine.every(c => c === "")) { // Validar si la fila está vacía
+            for (let j = 0; j < headers.length; j++) {
+                if (currentLine[cursor].startsWith('"')) {
+                    let field = currentLine[cursor].substring(1);
+
+                    while (!currentLine[cursor].endsWith('"')) {
+                        cursor++;
+                        field += `${separator}${currentLine[cursor]}`;
+                    }
+                    obj[headers[j]] = field.slice(0, -1);
+                } else {
+                    obj[headers[j]] = currentLine[cursor];
+                }
+                cursor++;
+            }
+            result.push(obj);
+        }
+    }
+    return result;
+}
+
+String.prototype.toTitleCase = function (capitalizeSingleWords = true) {
+    let text = this.toString();
+    let lastChar = text.slice(-1);
+    if (lastChar === " ") return text;
+    text = text.replace(/\b\w/g, l => l.toUpperCase());
+    let words = text.split(" ");
+    let result = "";
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(".")) {
+            result += words[i] + " ";
+        } else {
+            result += words[i].charAt(0) + words[i].slice(1).toLowerCase() + " ";
+        }
+    }
+    return result.trim();
+}
+
+let result = 'SOftware Integration CONSUlting E.I.R.L'.firstCharUp();
+console.log(result);
