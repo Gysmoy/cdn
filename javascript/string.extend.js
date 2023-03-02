2a797c40bf6f70acb9e5f51050fb0de9f7e63797
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-03-02 17:16:56 -0500

    Agregando un nuevo prototipo para poner las primeras palabras en mayúsculas

--- javascript/string.extend.js
+++ javascript/string.extend.js
@@ -1,11 +1,14 @@
 /**
- * Crea un objeto a partir de una cadena de texto que coincide con una expresión regular y una estructura de propiedades.
+ * Crea un objeto a partir de una cadena de texto que coincide con una expresión
+ * regular y una estructura de propiedades.
  *
  * @function
  * @memberof String.prototype
  * @param {RegExp} regex - La expresión regular para buscar en la cadena de texto.
- * @param {string} structure - Una cadena de texto con la estructura de propiedades del objeto separadas por comas.
- * @returns {Object|null} - Un objeto con las propiedades especificadas en la estructura o null si no se encuentra una coincidencia.
+ * @param {string} structure - Una cadena de texto con la estructura de propiedades
+ * del objeto separadas por comas.
+ * @returns {Object|null} - Un objeto con las propiedades especificadas en la
+ * estructura o null si no se encuentra una coincidencia.
  */
 String.prototype.matchAndCreateObject = function (regex, structure) {
     try {
@@ -23,15 +26,20 @@ String.prototype.matchAndCreateObject = function (regex, structure) {
 }
 
 /**
- * Divide una cadena de texto en un arreglo y opcionalmente crea objetos a partir de cada elemento utilizando una expresión regular y una estructura de propiedades.
+ * Divide una cadena de texto en un arreglo y opcionalmente crea objetos a partir
+ * de cada elemento utilizando una expresión regular y una estructura de propiedades.
  *
  * @function
  * @memberof String.prototype
- * @param {string} separator - El separador utilizado para dividir la cadena de texto en un arreglo.
+ * @param {string} separator - El separador utilizado para dividir la cadena de
+ * texto en un arreglo.
  * @param {object} [options] - Un objeto con opciones adicionales.
- * @param {RegExp} [options.regex] - La expresión regular utilizada para crear un objeto a partir de cada elemento.
- * @param {string} [options.structure] - Una cadena de texto con la estructura de propiedades de cada objeto separadas por comas.
- * @returns {Array} - Un arreglo con los elementos de la cadena de texto y, si se especifica la opción "regex", objetos creados a partir de cada elemento.
+ * @param {RegExp} [options.regex] - La expresión regular utilizada para crear un
+ * objeto a partir de cada elemento.
+ * @param {string} [options.structure] - Una cadena de texto con la estructura de
+ * propiedades de cada objeto separadas por comas.
+ * @returns {Array} - Un arreglo con los elementos de la cadena de texto y, si se
+ * especifica la opción "regex", objetos creados a partir de cada elemento.
  */
 String.prototype.split2 = function (separator, { regex, structure } = {}) {
     let array = this.split(separator);
@@ -52,8 +60,10 @@ String.prototype.split2 = function (separator, { regex, structure } = {}) {
  *
  * @function
  * @memberof String.prototype
- * @param {string} [separator=','] - El separador utilizado para separar los campos de la fila.
- * @param {number} [headers_pos=0] - La posición de la fila que contiene los encabezados de las columnas.
+ * @param {string} [separator=','] - El separador utilizado para separar los campos
+ * de la fila.
+ * @param {number} [headers_pos=0] - La posición de la fila que contiene los
+ * encabezados de las columnas.
  * @returns {Array} - Un arreglo de objetos JSON con los datos del archivo CSV.
  */
 String.prototype.csvToJson = function (separator = ',', headers_pos = 0) {
@@ -86,6 +96,20 @@ String.prototype.csvToJson = function (separator = ',', headers_pos = 0) {
     return result;
 }
 
+/**
+ * Capitaliza la primera letra de cada palabra de una cadena y deja en mayúscula las
+ * palabras que estén separadas por un punto.
+ * @method toTitleCase
+ * @memberof String.prototype
+ * @param {Boolean} [capitalizeSingleWords=true] - Si es verdadero, las palabras con
+ * una sola letra también se capitalizarán, de lo contrario, se mantendrán en minúsculas.
+ * @returns {string} Cadena con la primera letra de cada palabra en mayúscula y el resto
+ * en minúscula.
+ * @example
+ * var title = "software integration consulting a E.I.R.L";
+ * var capitalizedTitle = title.toTitleCase();
+ * console.log(capitalizedTitle); // "Software Integration Consulting a E.I.R.L"
+*/
 String.prototype.toTitleCase = function (capitalizeSingleWords = true) {
     let text = this.toString();
     let lastChar = text.slice(-1);
@@ -94,14 +118,17 @@ String.prototype.toTitleCase = function (capitalizeSingleWords = true) {
     let words = text.split(" ");
     let result = "";
     for (let i = 0; i < words.length; i++) {
-        if (words[i].includes(".")) {
-            result += words[i] + " ";
+        let word = words[i];
+        if (word.includes(".")) {
+            result += word + " ";
         } else {
-            result += words[i].charAt(0) + words[i].slice(1).toLowerCase() + " ";
+            let capitalize = capitalizeSingleWords || word.length > 1;
+            if (capitalize) {
+                result += word.charAt(0) + word.slice(1).toLowerCase() + " ";
+            } else {
+                result += word.toLowerCase() + " ";
+            }
         }
     }
     return result.trim();
 }
-
-let result = 'SOftware Integration CONSUlting E.I.R.L'.firstCharUp();
-console.log(result);
