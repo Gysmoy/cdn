ecd580b7e8109a93fb649dbf9c088ae935bcedd5
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2023-04-12 09:40:46 -0500

    JSON

--- php/JSON.php
+++ php/JSON.php
@@ -8,37 +8,30 @@
  */
 class JSON
 {
-
-    /* A recursive function that flattens a JSON object. */
-    static private function recurse($obj, $current, &$result)
-    {
-        foreach ($obj as $key => $value) {
-            $newKey = $current . "." . $key;
-            if ($current == '') {
-                $newKey = $key;
-            }
-            if (is_array($value)) {
-                foreach ($value as $index => $item) {
-                    if (is_array($item) || is_object($item)) {
-                        JSON::recurse($item, $newKey . "[$index]", $result);
-                    } else {
-                        $result[$newKey . "[$index]"] = $item;
-                    }
-                }
-            } elseif (is_object($value)) {
-                JSON::recurse($value, $newKey, $result);
-            } else {
-                $result[$newKey] = $value;
-            }
-        }
-    }
-
+    /**
+     * El `análisis de función pública estática` es un método que toma
+     * una cadena como entrada y devuelve una matriz. Utiliza la función
+     * `json_decode` para decodificar la cadena de entrada en una matriz.
+     * El segundo parámetro de `json_decode` se establece en `true`
+     * para garantizar que la salida sea una matriz asociativa. Luego,
+     * el método devuelve la matriz resultante.
+     */
     static public function parse(string $text): array
     {
         $array = json_decode($text, true);
         return $array;
     }
 
+    /**
+     * La `función pública estática stringify` es un método que toma
+     * un objeto como entrada y devuelve una representación de cadena
+     * JSON de ese objeto. Utiliza la función `json_encode` para codificar
+     * el objeto en una cadena JSON. Si el segundo parámetro `` se
+     * establece en `true`, utiliza la opción `JSON_PRETTY_PRINT` para
+     * formatear la cadena JSON con sangría y saltos de línea para
+     * mejorar la legibilidad. Luego, el método devuelve la cadena
+     * JSON resultante.
+     */
     static public function stringify(mixed $object, bool $pretty = false): string
     {
         if ($pretty) {
@@ -50,9 +43,13 @@ class JSON
     }
 
     /**
-     * Método que verifica si un string es un JSON válido.
-     * @param text - Texto a verificar.
-     * @return bool - valor booleano.
+     * El método de `función pública estática analizable` toma un
+     * parámetro `` mixto y devuelve un valor booleano. Utiliza la función
+     *  `json_decode` para decodificar la cadena JSON y luego verifica
+     * si hubo algún error durante el proceso de decodificación usando
+     * la función `json_last_error`. Si no hubo ningún error, devuelve
+     * `true`, lo que indica que la cadena JSON se puede analizar. De
+     * lo contrario, devuelve `falso`.
      */
     static public function parseable(mixed $text): bool
     {
@@ -61,45 +58,99 @@ class JSON
     }
 
     /**
-     * Recibe un objeto y retorna un nuevo objeto con todas las claves aplanadas.
-     * @param object - El objeto que va a ser aplanado.
-     * @param [prev] - La clave previa.
-     * @return array un objeto con las claves y valores del objeto original, pero con las claves aplanadas.
+     * El método `static public function flatten` toma un objeto como
+     * entrada y devuelve un nuevo objeto con todas las claves aplanadas.
+     * Utiliza array_reduce para iterar a través de cada clave en el
+     * objeto de entrada y aplanar recursivamente cualquier matriz u
+     * objeto anidado. El parámetro `` especifica el separador que se
+     * usará entre las claves planas, mientras que `` permite agregar
+     * un prefijo a todas las claves. El parámetro `` determina si la
+     * última matriz en una estructura anidada también debe aplanarse
+     * o no. El método devuelve el objeto aplanado como una matriz.
      */
-    static public function flatten(mixed $obj): array
+    static public function flatten($obj, $notation = '.', $prefix = '', $flattenLastArray = true)
     {
-        $result = array();
-        JSON::recurse($obj, "", $result);
+        return array_reduce(array_keys($obj), function ($acc, $k) use ($notation, $prefix, $flattenLastArray, $obj) {
+            $pre = strlen($prefix) ? $prefix . $notation : '';
+            if (is_array($obj[$k])) {
+                if ($flattenLastArray && !is_array($obj[$k][0])) {
+                    foreach ($obj[$k] as $i => $item) {
+                        $acc[$pre . $k . '[' . $i . ']'] = $item;
+                    }
+                } else {
+                    $acc[$pre . $k] = $obj[$k];
+                }
+            } else if (is_object($obj[$k])) {
+                $acc += JSON::flatten((array) $obj[$k], $notation, $pre . $k, $flattenLastArray);
+            } else {
+                $acc[$pre . $k] = $obj[$k];
+            }
+            return $acc;
+        }, []);
+    }
+
+    /**
+     * La función `unflatten` toma una matriz u objeto aplanado y la
+     * vuelve a convertir en una matriz u objeto anidado. Lo hace
+     * iterando a través de cada clave en la matriz aplanada, dividiéndola
+     * en sus claves anidadas usando la notación especificada (el valor
+     * predeterminado es ".") y luego creando matrices u objetos anidados
+     * según sea necesario para reconstruir la estructura original. Si
+     * una clave contiene corchetes (por ejemplo, "foo[0]"), se trata
+     * como un índice de matriz y la matriz anidada correspondiente se
+     * crea o amplía según sea necesario. La función devuelve la matriz
+     * u objeto anidado reconstruido.
+     */
+    static public function unflatten($obj, $notation = '.')
+    {
+        $result = [];
+        foreach ($obj as $key => $value) {
+            $keys = explode($notation, $key);
+            $cur = &$result;
+            foreach ($keys as $i => $prop) {
+                if (strpos($prop, '[') !== false && strpos($prop, ']') === strlen($prop) - 1) {
+                    $index = intval(substr($prop, strpos($prop, '[') + 1, -1));
+                    $prop = substr($prop, 0, strpos($prop, '['));
+                    if (!isset($cur[$prop])) {
+                        $cur[$prop] = [];
+                    }
+                    while (count($cur[$prop]) < $index) {
+                        $cur[$prop][] = [];
+                    }
+                    if ($i === count($keys) - 1) {
+                        $cur[$prop][$index] = $value;
+                    } else {
+                        if (!isset($cur[$prop][$index])) {
+                            $cur[$prop][$index] = [];
+                        }
+                        $cur = &$cur[$prop][$index];
+                    }
+                } else {
+                    if ($i === count($keys) - 1) {
+                        $cur[$prop] = $value;
+                    } else {
+                        if (!isset($cur[$prop])) {
+                            $cur[$prop] = [];
+                        }
+                        $cur = &$cur[$prop];
+                    }
+                }
+            }
+            unset($cur);
+        }
         return $result;
     }
 
-    /* Restoring the flattened object. */
-    static public function restore($object, $notation = '.')
+    /**
+     * El método `take` es una función pública estática que toma una
+     * matriz `` y una cantidad `` como parámetros. Luego devuelve una
+     * nueva matriz que contiene los primeros `` elementos de la matriz
+     * original ``. Lo logra mediante el uso de la función `array_slice`,
+     * que devuelve una parte de una matriz. En este caso, devuelve
+     * los primeros `` elementos del array ``.
+     */
+    static public function take($obj, $quantity)
     {
-        $restored = array();
-        foreach ($object as $key => $value) {
-            $keys = explode($notation, $key);
-            if (count($keys) > 1) {
-                $key = array_shift($keys);
-                $newkey = implode($notation, $keys);
-                $kv = $restored[$key] ?? array();
-                $kv[$newkey] = $value;
-                $restored[$key] = JSON::restore($kv, $notation);
-            } else {
-                $restored[$key] = $value;
-            }
-        }
-        return $restored;
-    }
-
-    static public function fromXML(string $xml): array
-    {
-        $xml = preg_replace('/<([a-z]+):([a-z]+)/i', '<$1_$2', $xml);
-        $xml = preg_replace('/<\/([a-z]+):([a-z]+)>/i', '</$1_$2>', $xml);
-
-        $xml_object = simplexml_load_string($xml);
-        $json_array = json_decode(json_encode($xml_object), true, JSON_THROW_ON_ERROR);
-
-        return $json_array;
+        return array_slice($obj, 0, $quantity);
     }
 }
--- php/gfetch.php
+++ php/gfetch.php
@@ -1,23 +1,26 @@
 <?php
 
-class gFetch
+class Fetch
 {
     private $curl;
     private string $response;
     public bool $ok;
     public string $status;
     public string $contentType;
-    function __construct($url, $options = [
-        'method' => 'GET',
-        'body' => [],
-        'headers' => []
-    ])
+
+    function __construct(string $url, array $options = [])
     {
+        $options['headers'] = $options['headers'] ?? [];
+        $options['method'] = $options['method'] ?? 'GET';
+        $options['body'] = $options['body'] ?? [];
+
         $this->curl = curl_init();
+
         $headers = [];
         foreach ($options['headers'] as $key => $value) {
             $headers[] = "$key: $value";
         }
+
         curl_setopt_array($this->curl, [
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
@@ -26,55 +29,38 @@ class gFetch
             CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => $options['method'] ?? 'GET',
-            CURLOPT_POSTFIELDS => $options['method'] != 'GET' ? json_encode($options['body'] ?? []) : null,
-            CURLOPT_HTTPHEADER => $options['headers'],
+            CURLOPT_CUSTOMREQUEST => $options['method'],
+            CURLOPT_POSTFIELDS => $options['method'] != 'GET' ? json_encode($options['body']) : null,
+            CURLOPT_HTTPHEADER => $headers,
         ]);
+
         $this->response = curl_exec($this->curl);
-        $this->status = curl_getinfo($this->curl, CURLINFO_RESPONSE_CODE);
-        $this->ok = $this->status >= 200 && $this->status < 300 ? true : false;
-        $this->contentType = curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE);
+        $this->status = (string) curl_getinfo($this->curl, CURLINFO_RESPONSE_CODE);
+        $this->ok = $this->status >= 200 && $this->status < 300;
+        $this->contentType = (string) curl_getinfo($this->curl, CURLINFO_CONTENT_TYPE);
+
+        if ($this->response === false) {
+            throw new Exception(curl_error($this->curl), curl_errno($this->curl));
+        }
     }
 
     public function text(): string
     {
         return $this->response;
     }
+
     public function json(): array
     {
         return json_decode($this->response, true);
     }
+
     public function blob(): string
     {
         return $this->response;
     }
+
     function __destruct()
     {
         curl_close($this->curl);
     }
 }
-
-$res = new gFetch('http://localhost:8081/rimac/proxy', [
-    'method' => 'POST',
-    'body' => [
-        'url' => 'https://api.lolhuman.xyz/api/ytplay2?apikey=BrunoSobrino&query=avicii without you',
-        'options' => [
-            'method' => 'GET'
-        ]
-    ],
-    'headers' => [
-        'Content-Type: application/json',
-        'Accept: application/json'
-    ]
-]);
-
-$ok = $res->ok;
-$status =  $res->status;
-$contentType = $res->contentType;
-$json = $res->text();
-
-echo "status: {$ok}
-status: {$status}
-content-type: {$contentType}
-json: {$json}
-";
