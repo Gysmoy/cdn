ec9677ed43fb140fbc48c0c31d80f7fe38bc80b9
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-05-30 14:15:30 -0500

    NTS

--- extends/get.extend.js
+++ extends/get.extend.js
@@ -2,3 +2,18 @@ const GET = {}
 
 let urlParams = new URLSearchParams(location.search);
 [...urlParams].forEach(get => GET[get[0]] = get[1]);
+
+class URI {
+  static redirect = (uri, params) => {
+    const urlParams = new URLSearchParams(params);
+    document.location.href = `${uri}?${urlParams}`
+  }
+  static hash = (hash) => {
+    document.location.hash = hash
+  }
+  static alter = (uri, params) => {
+    const history = window.history;
+    const urlParams = new URLSearchParams(params);
+    history.pushState(null, null, `${uri}?${urlParams}`);
+  }
+}
